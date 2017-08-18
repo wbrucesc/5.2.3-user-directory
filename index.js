37514baa8a4aae45914b563f0516ad33990bba44
Will Bruce <willtiger13@gmail.com>
2017-08-18 11:02:24 -0400

    cleared up comments

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const data = require('./data');
 
-// console.log(data.users[0].address.country);
-
 
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -11,41 +9,23 @@ app.set('view engine', 'handlebars');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', {bots: data});
+  res.render('index', {bots: data});              //displays the user's info on index page
 });
 
 app.get('/user/:name', (req, res) =>{
-  let userName = req.params.name;
+  let userName = req.params.name;                 //allows you to target a specific user by their name
   let targetUser;
   data.users.forEach((user) =>{
     if(user.name == userName){
       targetUser = user;
     }
   });
-  res.render('profile', {user: targetUser});
+  res.render('profile', {user: targetUser});      //if name clicked renders profile with user info
 });
 
-app.get('/request', (req, res) =>{
-  res.render('request'); 
+app.get('/request', (req, res) =>{                //renders request success page if connect button clicked
+  res.render('request');
 });
-
-// app.get('/user/:job', (req, res) => {
-//   let userJob = req.params.job;
-//   let targetJob;
-//     data.forEach((user) =>{
-//       if(user.job == userJob){
-//         targetJob = job;
-//       }
-//     });
-//       if (targetJob === 'null'){
-//         res.send("Currently seeking employment.");
-//       }
-//
-// });
-
-
-
-
 
 
 
